07c69f21fb807b3942ec156c5c499134e1e65e47
T1mS22 <tim220200.ts@gmail.com>
2024-03-11 21:56:41 +0100

    added theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,23 @@ const config: Config = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FF7A00",
+          secondary: "#323232",
+          accent: "#FF5714",
+          neutral: "#B8B8B8",
+          "base-100": "#F0F0F0",
+          info: "#17A2B8",
+          success: "#28A745",
+          warning: "#FFC107",
+          error: "#DC3545",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
 export default config;
