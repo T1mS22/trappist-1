83a8177072d924bcac54aa16fecc3ef502be3f7d
T1mS22 <tim220200.ts@gmail.com>
2024-03-17 13:46:51 +0100

    basic infopage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Tim Schneider</title>
+        <meta property="og:title" content="Tim Schneider" key="title" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
